acae723a7bc9779d45ff46e09b934f9d6467cafe
vandit15 <jainvandit15@gmail.com>
2017-03-19 02:11:06 +0530

    extention of features

--- server.R
+++ server.R
@@ -1,35 +1,81 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
+## loading libraries
 library(shiny)
 library(ggplot2)
+library(plotly)
+
+## load data
 data <- read.csv("data.csv")
 
+## main server function
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-    print(input$part)
+  ## function for the plot on first tab
+  output$distPlot <- renderPlotly({
+    ## range of values from dataset taken as slider input
     range <- seq(input$part[1],input$part[2])
     temp <- data[range,]
-    plot <- ggplot()
+    
+    ## if x axis is not given to be observer
     if(input$x!="Observer"){
-      finalPlot <- plot + geom_point(data = data[range,],aes_string(x = input$x,y = input$y,color="Observer"))
+      ##basic plot structure
+      plot <- ggplot()
+      # adding scatter plot with given x and y axis
+      p <- plot + geom_point(data = data[range,],aes_string(x = input$x,y = input$y,color="Observer"),show.legend=TRUE,size=3)
+      #aading layer of plotly
+      finalPlot <- ggplotly(p)
     } else {
-      finalPlot <- plot + geom_bar(data=data[range,],aes(factor(Observer)))
+      # plotting basic plot structure with aesthetics and data
+      plot <- ggplot(data=data[range,],aes_string(x = "Observer",y = input$y),fill = "red",show.legend = TRUE)
+      # building the boxplot
+      finalPlot <- plot + geom_boxplot(varwidth = T)
+      ## adding layer of plotly
+      finalPlot <- ggplotly(finalPlot)
     }
-
-    # generate bins based on input$bins from ui.R
-    #x    <- faithful[, 2]
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
     return(finalPlot)
 
+  })
+  
+  
+  ## function for the plot on the seconf tab
+  output$lineplot <- renderPlotly({
+   ## range of values from dataset taken as slider input
+   range <- seq(input$part[1],input$part[2])
+   ## case when input on x-axis is not observer
+   if(input$x!="Observer"){
+     # basic plot structure
+     plot <- ggplot()
+     # adding lineplot to the basic structre
+     
+     finalPlot <- plot + geom_line(data = data[range,],aes_string(x = input$x,y = input$y,color = "Observer"),show.legend=TRUE)
+     ## adding layer of plotly
+     finalPlot <- ggplotly(finalPlot)  
+     } else {
+     # plotting basic structure and mention data which is going to be inherited
+     plot <- ggplot(data = data[range,],aes_string(x = input$y))
+     # building a density plot
+     finalPlot <- plot + geom_density(aes(fill = factor(Observer)),alpha=0.8)
+     ## ading layer of plotly
+     finalPlot <- ggplotly(finalPlot)
+   }
+   return(finalPlot)
+  })
+  
+  
+  ## function for the plot on the seconf tab
+  output$threedplot <- renderPlotly({
+    ## range of values from dataset taken as slider input
+    range <- seq(input$part[1],input$part[2])
+    ## case when input on x-axis is not observer
+    #if(input$x!="Observer"){
+    finalPlot <- plot_ly(x = data[range,input$x],y = data[range,input$y],z = data[range,input$z],color = data[range,'Observer']) %>%
+      layout(title = "3-d representation",
+             scene = list(
+               xaxis = list(title= input$x),
+               yaxis = list(title= input$y),
+               zaxis = list(title= input$z)
+             ))
+    return(finalPlot)
   })
 
 })
--- ui.R
+++ ui.R
@@ -1,42 +1,51 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-
+library(plotly)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Visualisation of Avian Habitat Data"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
+      ## slider input for slicing dataset
       sliderInput("part",
                   "Number of observations from the dataset:",
                   min = 1,
                   max = 1070,
                   value = c(1,1070)),
+      ## drop down input for x axis
       selectInput("x", 
                   label = "Choose x axis for the graph",
                   choices = c("Observer", "Subpoint",
                               "VOR", "PDB","DBHt","PW","WHt","PE","EHt",
                               "PA","AHt","PH","HHt","PL","LHt","PB"),
                   selected = "Observer"),
+      ## drop down input for y axis
       selectInput("y", 
                   label = "Choose y axis for the graph",
                   choices = c( "Subpoint",
                               "VOR", "PDB","DBHt","PW","WHt","PE","EHt",
                               "PA","AHt","PH","HHt","PL","LHt","PB"),
+                  selected = "Subpoint"),
+      ## drop down input for z axis
+      selectInput("z", 
+                  label = "Choose z axis in case of 3-d graph",
+                  choices = c( "Subpoint",
+                               "VOR", "PDB","DBHt","PW","WHt","PE","EHt",
+                               "PA","AHt","PH","HHt","PL","LHt","PB"),
                   selected = "Subpoint")
+      
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      tabsetPanel(
+      tabPanel("Plot1",plotlyOutput("distPlot")),
+      tabPanel("Plot2",plotlyOutput("lineplot")),
+      tabPanel("Plot3",plotlyOutput("threedplot"))
+    )
     )
   )
 ))
